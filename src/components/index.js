2065791ecf953db9780f1a9ee93832175d08e7de
cof <merfais.bwq@163.com>
2024-11-28 22:14:57 +0800

    modify export module

--- src/components/index.js
+++ src/components/index.js
@@ -1,34 +1,11 @@
 import GridItem from './grid-item.vue';
 import GridLayout from './grid-layout.vue';
-// import ResponsiveGridLayout from './ResponsiveGridLayout.vue';
 
-const VueGridLayout = {
-  // ResponsiveGridLayout,
-  GridLayout,
-  GridItem,
-};
-
-export function install(Vue) {
+export default function install(app) {
   if (install.installed) return;
   install.installed = true;
-  Object.keys(VueGridLayout).forEach(name => {
-    Vue.component(name, VueGridLayout[name]);
-  });
+  app.component('GridLayout', GridLayout);
+  app.component('GridItem', GridItem);
 }
 
-const plugin = {
-  install,
-};
-
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
-export default VueGridLayout;
 export { GridLayout, GridItem };
--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,37 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueDevTools from 'vite-plugin-vue-devtools';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import eslint from '@rollup/plugin-eslint';
+import pkg from './package.json';
+
+function banner() {
+  return {
+    name: 'banner',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      const banner = `/**\n * ${pkg.name} ${pkg.version}\n`
+        + ` * ${pkg.author}\n * ${pkg.homepage}\n */\n`;
+
+      for (const module of Object.values(bundle)) {
+        if (module.type === 'chunk') {
+          module.code = banner + module.code;
+        }
+      }
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
     eslint({
       include: ['./src/**/*.js', './src/**/*.vue'],
     }),
-    cssInjectedByJsPlugin(),
+    libInjectCss(),
+    banner(),
   ],
   resolve: {
     alias: {
@@ -24,7 +41,6 @@ export default defineConfig(() => ({
     },
   },
   build: {
-    minify: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/components/index.js'),
@@ -36,12 +52,28 @@ export default defineConfig(() => ({
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
-      output: {
+      output: [{
+        format: 'es',
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
+        globals: { vue: 'Vue' },
+      }, {
+        name: 'VueGridLayout',
+        format: 'iife',
+        exports: 'named',
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: { vue: 'Vue' },
+      }, {
+        name: 'VueGridLayout',
+        format: 'umd',
+        exports: 'named',
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: { vue: 'Vue' },
+      }, {
+        format: 'cjs',
+        exports: 'named',
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: { vue: 'Vue' },
+      }],
     },
   },
 }));
